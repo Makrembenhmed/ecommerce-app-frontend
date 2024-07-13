7b88e8d5b1a883df104a061ba7c20158f96a5dd8
makrem <makrembhmed@gmail.com>
2024-07-13 17:16:05 +0100

    correction 2

--- pages/api/checkout.jsx
+++ pages/api/checkout.jsx
@@ -1,6 +1,6 @@
 import { mongooseConnect } from "@/lib/mongoose";
 import { Order } from "@/models/Order";
-import { Product } from "@/models/Product";
+import { Product } from "@/models/product";
 const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 export default async function handler(req, res) {
